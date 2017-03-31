c20b1a9c9122118ab3aa6dd331438fc057aefe2c
Efthimios Konstantinidis <efkonst@gmail.com>
2017-03-31 13:50:31 +0300

    edit constructor

--- Display.py
+++ Display.py
@@ -22,29 +22,21 @@ class Display:
                 self.SPI_PORT = 0
                 self.SPI_DEVICE = 0
                 self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=self.RST)
-                print "initialized"
-
-        def printR(self):
                 self.disp.begin()
                 self.disp.clear()
-                self.disp.display()
-                image = Image.new('1', (self.width, self.height))
-                draw = ImageDraw.Draw(image)
-                draw.rectangle((0,0,self.width,self.height), outline=0, fill=1)
-                self.disp.image(image)
-                self.disp.display()
+                self.image = Image.new('1', (self.width, self.height))
+                print "initialized"
 
-        def createLines(self):
-            image = Image.new('1', (self.width, self.height))
-            draw = ImageDraw.Draw(image)
+        
+        def drawLines(self):
+            draw = ImageDraw.Draw(self.image)
             for i in range(1,self.height,2):
                 draw.line((0, i, self.width, i), fill=0)
-            return image
+            printImage()
         
-        def printImage(self,img):
-            self.disp.begin()
+        def printImage(self):
             self.disp.clear()
             self.disp.display()
-            self.disp.image(img)
+            self.disp.image(self.image)
             self.disp.display()
 
