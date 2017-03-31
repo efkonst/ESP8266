c4ec859d52b6f01233f0c9b6c73029d6797bb5fb
Efthimios Konstantinidis <efkonst@gmail.com>
2017-03-31 14:44:30 +0300

    add parser

---
+++ Webparser.py
@@ -0,0 +1,38 @@
+import urllib2
+import json,sys,csv
+
+
+class Webparser:
+    def __init__(self,stock_name):
+        self.stockname = stock_name 
+    
+    
+    
+    def getmaxprices(self):
+        url = 'https://www.google.com/finance/historical?output=csv&q='+self.stockname
+        proxy = urllib2.ProxyHandler({'https': '10.159.68.132:8080'})
+        opener = urllib2.build_opener(proxy)
+        urllib2.install_opener(opener)
+        response = urllib2.urlopen(url)
+        cr = csv.reader(response)
+        csvlist = list(cr)
+        pricelist = (csvlist[1:128])[::-1]
+        maxlist =  [x[4] for x in pricelist]
+        return maxlist
+
+        
+
+
+
+
+#w = Webparser("NOK") 
+#w.getmaxprices()
+
+'''
+stockname = "NOK"
+
+url = 'https://www.google.com/finance/historical?output=csv&q='+stockname
+response = urllib2.urlopen(url)
+cr = csv.reader(response)
+csvlist = list(cr)
+'''
