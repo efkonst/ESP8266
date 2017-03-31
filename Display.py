8cfee7fbf0fa61fbfc68b35052ded86b963a8dd9
Efthimios Konstantinidis <efkonst@gmail.com>
2017-03-31 06:29:15 +0000

    Add files to repo

---
+++ Display.py
@@ -0,0 +1,35 @@
+import time
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import urllib2
+import json,sys,csv
+import datetime 
+from datetime import timedelta
+# Raspberry Pi pin configuration:
+
+class Display:
+        def __init__(self):
+                print "initializing"
+                self.width = 128
+                self.height = 64
+                self.RST = 24
+                self.DC = 23
+                self.SPI_PORT = 0
+                self.SPI_DEVICE = 0
+                self.disp = Adafruit_SSD1306.SSD1306_128_64(rst=self.RST)
+                print "initialized"
+
+        def printR(self):
+                self.disp.begin()
+                self.disp.clear()
+                self.disp.display()
+                image = Image.new('1', (self.width, self.height))
+                draw = ImageDraw.Draw(image)
+                draw.rectangle((0,0,self.width,self.height), outline=0, fill=1)
+                self.disp.image(image)
+                self.disp.display()
---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import Display
+
+print "hello :-)"
+d = Display.Display()
+d.printR(1)
+print "end"
+
